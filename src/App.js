b4aa670741076d5d8ef7cd957562ee3d6780adb6
rysyaa <captain0demon@gmail.com>
2023-06-10 20:29:31 +0600

    header and section1 add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ColectionCard from "./components/ColectionCard";
 import Cards from "./components/Cards";
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 const App = () => {
   return <div>
     {/* <ColectionCard/> */}
     {/* <Cards/> */}
+    <Header/>
+    <Main/>
   </div>;
 };
 
