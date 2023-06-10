9441a9bbcaa8ad934b55e40a3c4453cec2b1af72
rysyaa <captain0demon@gmail.com>
2023-06-10 18:52:47 +0600

    cards and colection add

--- src/components/ColectionCard.jsx
+++ src/components/ColectionCard.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import "./styles/ColectionCard.css";
+import testPhoto from "../assets/testPhoto.JPG"
+import fav from "../assets/fav.png"
 
 const ColectionCard = () => {
   return (
-    <div>ColectionCard</div>
-  )
-}
+    <div className="colection__container">
+      <div className="colection__block">
+        <img
+          src={testPhoto}
+          alt=""
+        />
+        <img id="colection__fav" src={fav} alt="" />
+        <div className="colection__textBottom">
+          <h5>Lorem ipsum dolor sit.</h5>
+          <h5>50, 50</h5>
+          <span>Delicous Demi</span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default ColectionCard
+export default ColectionCard;
