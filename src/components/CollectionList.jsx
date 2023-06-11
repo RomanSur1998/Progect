b43eacb4913fae9b3e9694fb15f194b408b4becf
RomanSur1998 <123935997+RomanSur1998@users.noreply.github.com>
2023-06-11 13:26:55 +0600

    added save block

--- src/components/CollectionList.jsx
+++ src/components/CollectionList.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import ColectionCard from "./ColectionCard";
 
 const CollectionList = () => {
-  return <div>CollectionList</div>;
+  return (
+    <div className="collectionContainer">
+      <ColectionCard />
+    </div>
+  );
 };
 
 export default CollectionList;
--- src/components/DetailsCard.jsx
+++ src/components/DetailsCard.jsx
@@ -1,17 +1,23 @@
 import React from "react";
+import DetailsPhoto from "../assets/3-pack-port_800x.webp";
 
 const DetailsCard = () => {
   return (
-    <div>
+    <>
       <div>
         <div>
-          <img src="" alt="" />
+          <img src={DetailsPhoto} alt="" />
         </div>
       </div>
       <div>
-        <p>JОписание </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+          earum nemo quidem doloremque cumque animi hic at quisquam nobis,
+          libero sapiente aspernatur quae, dolore expedita temporibus ex fuga
+          officiis in?{" "}
+        </p>
       </div>
-    </div>
+    </>
   );
 };
 
